d872c798548b26f372a1949d51a28fde1880a55b
Yaroslaff Fedin <me@inviz.ru>
2010-09-13 18:12:26 +0800

    Add Date.js file haders to make packager happy

---
+++ Source/Widget/Input/Date.js
@@ -0,0 +1,26 @@
+/*
+---
+ 
+script: Date.js
+ 
+description: Date picker
+ 
+license: MIT-style license.
+
+authors: Yaroslaff Fedin
+ 
+requires:
+- ART.Widget.Input
+
+provides: [ART.Widget.Input.Date]
+ 
+...
+*/
+
+ART.Widget.Input.Date = new Class({
+  Includes: [
+    Widget.Trait.Input
+  ],
+  
+  name: 'input'
+});
