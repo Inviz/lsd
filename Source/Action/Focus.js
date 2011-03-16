a585454b496293be6b00a8b52997bb4edb167596
Yaroslaff Fedin <me@inviz.ru>
2011-03-16 11:43:06 +0800

    Add focus action outline

---
+++ Source/Action/Focus.js
@@ -0,0 +1,19 @@
+/*
+---
+ 
+script: Focus.js
+ 
+description: Brings attention to element
+ 
+license: Public domain (http://unlicense.org).
+
+authors: Yaroslaff Fedin
+ 
+requires:
+  - LSD.Action
+ 
+provides:
+  - LSD.Action.Focus
+ 
+...
+*/
