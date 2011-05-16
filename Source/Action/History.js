9740cf19e32a1faec844ce21aa3fe7f183475a8b
Andrey Koppel <akoppela@gmail.com>
2011-05-16 14:47:20 +0800

    + History Action

---
+++ Source/Action/History.js
@@ -0,0 +1,28 @@
+/*
+---
+ 
+script: History.js
+ 
+description: History Action Management.
+ 
+license: Public domain (http://unlicense.org).
+
+authors: Yaroslaff Fedin
+ 
+requires:
+  - LSD.Action
+  - History/History
+ 
+provides:
+  - LSD.Action.History
+ 
+...
+*/
+
+LSD.Action.History = LSD.Action.build({
+  enable: function(target, content) {
+    var url = target.getAttribute('src')|| target.getAttribute('action') || target.getAttribute('href');
+    console.info(url);
+    History[content && typeof(content) == 'string' ? content : 'push'].apply(History, [url]);
+  }
+});
