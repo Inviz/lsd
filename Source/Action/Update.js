2cc98f6cc2d0cd785108f24709448130f0766844
Andrey Koppel <akoppela@gmail.com>
2011-07-06 13:57:32 +0800

    fixed Action.Update

--- Source/Action/Update.js
+++ Source/Action/Update.js
@@ -23,13 +23,15 @@ provides:
 */
 
 LSD.Action.Update = LSD.Action.build({
+  container: true,
+  
   enable: function(target, content) {
     if (!content) return LSD.warn('Update action did not recieve content');
     var widget = LSD.Module.DOM.find(target);
     var fragment = document.createFragment(content);
     var children = LSD.slice(fragment.childNodes);
     var element = target.lsd ? target.toElement() : target;
-    var container = (target.lsd || (widget.element == target && widget)) ? widget.getWrapper() : element;
+    var container = (target.lsd || (widget.element == target && widget)) ? widget[this.options.container ? 'toWrapper' : 'toElement']() : element;
     var args = [container, (widget.element == element ? widget.parentNode : widget), fragment, children, content];
     this.options.update.apply(this, args);
   },
