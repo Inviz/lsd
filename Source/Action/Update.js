5d9dda1e28a35421fb8b2702542de896adcb3952
Yaroslaff Fedin <invizko@gmail.com>
2011-09-13 16:24:58 +0800

    Make Update treat widgets just as elements, fix two typos

--- Source/Action/Update.js
+++ Source/Action/Update.js
@@ -23,8 +23,6 @@ provides:
 */
 
 LSD.Action.Update = LSD.Action.build({
-  container: true,
-  
   enable: function(target, content) {
     if (!content) return LSD.warn('Update action did not recieve content');
     if (content.charAt) {
@@ -37,14 +35,19 @@ LSD.Action.Update = LSD.Action.build({
         fragment.appendChild(content);
       } else for (var i = 0, child; child = children[i++];) fragment.appendChild(child);
     }
-    var widget = LSD.Module.DOM.find(target);
-    if (widget.element == target) {
-      parent = widget.parentNode;
-      var element = target;
-      target = parent;
+    if (target.lsd) {
+      var widget = target;
+      var element = widget.toElement();
+      var parent = target.parentNode;
     } else {
-      var element = target, parent = widget;
-      widget = null;
+      var widget = LSD.Module.DOM.find(target);
+      if (widget.element == target) {
+        parent = widget.parentNode;
+        var element = target;
+      } else {
+        var element = target, parent = widget;
+        widget = null;
+      }
     }
     this.options.update.apply(this, [element, widget, parent, fragment, children]);
   },
@@ -68,9 +71,9 @@ LSD.Action.Append = LSD.Action.build({
 LSD.Action.Replace = LSD.Action.build({
   enable: LSD.Action.Update.prototype.options.enable,
 
-  update: function(element, widget, parent, fragment) {
+  update: function(element, widget, parent, fragment, children) {
     var parentNode = element.parentNode
-    parentNode.replaceChild(fragment, element, children);
+    parentNode.replaceChild(fragment, element);
     parent.removeLayout(null, widget || element);
     parent.addLayout(null, children, parentNode);
   }
@@ -80,7 +83,7 @@ LSD.Action.Before = LSD.Action.build({
   enable: LSD.Action.Update.prototype.options.enable,
 
   update: function(element, widget, parent, fragment, children) {
-    target.parentNode.insertBefore(fragment, element);
+    element.parentNode.insertBefore(fragment, element);
     parent.addLayout(null, children, element.parentNode, {before: element});
   }
 });
