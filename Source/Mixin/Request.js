c9941dd87f7260c4a4f8c6f842e2573176ecb949
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-10-12 21:11:27 +0200

    Handling one of the edge cases for LSD.Object.toData Right now it can't handle nil. I'm not absolutely sure what'd be an expected output in case of nil.

--- Source/Mixin/Request.js
+++ Source/Mixin/Request.js
@@ -1,22 +1,22 @@
 /*
 ---
- 
+
 script: Request.js
- 
+
 description: Make various requests to back end
- 
+
 license: Public domain (http://unlicense.org).
- 
+
 requires:
   - LSD.Mixin
   - Core/Request
   - Ext/Request.Form
   - Ext/Request.Auto
   - Ext/document.createFragment
-  
-provides: 
+
+provides:
   - LSD.Mixin.Request
- 
+
 ...
 */
 
@@ -31,11 +31,11 @@ LSD.Mixin.Request = new Class({
         submit: function() {
           return this.send.apply(this, arguments);
         },
-        
+
         cancel: function() {
           return this.stop()
         },
-        
+
         getCommandAction: function() {
           if (!this.isRequestURLLocal()) return 'submit';
         },
@@ -46,9 +46,9 @@ LSD.Mixin.Request = new Class({
       }
     }
   },
-  
+
   send: function() {
-    var data = this.getRequestData && this.getRequestData() || null;
+    var data = this.getRequestData && this.getRequestData() || {};
     var options = Object.merge({}, this.options.request, {data: LSD.Object.toObject(data), url: this.getRequestURL(), method: this.getRequestMethod()});
     for (var i = 0, j = arguments.length, arg, opts; i < j; i++) {
       var arg = arguments[i];
@@ -63,7 +63,7 @@ LSD.Mixin.Request = new Class({
     this.fireEvent('send', options);
     return this.request.send(options);
   },
-  
+
   stop: function() {
     if (this.request) {
       this.request.cancel();
@@ -72,7 +72,7 @@ LSD.Mixin.Request = new Class({
     this.fireEvent('stop');
     return this;
   },
-  
+
   getRequest: function(options) {
     var type = (options && options.type) || this.getRequestType();
     var request = this[type == 'xhr' ? 'getXHRRequest' : 'getFormRequest'](options);
@@ -83,35 +83,35 @@ LSD.Mixin.Request = new Class({
       complete: function() {
         this.idle();
         if (request.isSuccess && request.isSuccess() && this.getCommandAction && this.getCommandAction() == 'submit')
-          if (this.chainPhase == -1 
-          || (this.chainPhase == this.getActionChain().length - 1) 
+          if (this.chainPhase == -1
+          || (this.chainPhase == this.getActionChain().length - 1)
           || ((this.currentChain[this.chainPhase].action == 'submit') && this.currentChain[this.chainPhase] == null))
             this.eachLink('optional', arguments, true);
       }.bind(this)
     });
     return request;
   },
-  
+
   getXHRRequest: function(options) {
     return new Request.Auto(options);
   },
-  
+
   getFormRequest: function(options) {
     return new Request.Form(options);
   },
-  
+
   getRequestType: function() {
     return this.attributes.transport || this.options.request.type;
   },
-  
+
   getRequestMethod: function() {
     return this.attributes.method || this.options.request.method;
   },
-  
+
   getRequestURL: function() {
     return this.attributes.href || this.attributes.src || this.attributes.action;
   },
-  
+
   isRequestURLLocal: function(base, host) {
     if (!host) host = location.host;
     if (!base) base = location.pathname;
