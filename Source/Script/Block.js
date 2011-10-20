f802f20bf4b5370c49f7c9c5e6613dce5980199d
Yaroslaff Fedin <invizko@gmail.com>
2011-10-20 23:25:51 +0800

    Implement unyielding properly in Blocks. Blocks now detach themselves properly

--- Source/Script/Block.js
+++ Source/Script/Block.js
@@ -42,27 +42,62 @@ LSD.Script.Block = function(input, source, output, locals) {
 }
 
 LSD.Script.Block.prototype = Object.append({}, LSD.Script.Function.prototype, {
-  invoke: function() {
-    this.invoked = true;
-    if (this.locals) {
-      for (var local, i = 0; local = this.locals[i]; i++)
-        this.variables.set(local.name, arguments[i]);
+  yield: function(keyword, args, callback, index, old) {
+    switch (keyword) {
+      case 'yield':
+        if (!this.yields) this.yields = {};
+        var block = this.yields[old == null ? index : old];
+        if (old != null) this.yields[index] = block;
+        if (!block) block = this.yields[index] = new LSD.Script.Block(this.input, this.source, null, this.locals);
+        var invoked = block.invoked;
+        block.yielder = callback;
+        block.invoke(args, true);
+        if (invoked) block.invoke(invoked, false);
+        callback.block = block;
+        return block;
+      case 'unyield':
+        callback.call(this, null, args[0], args[1], args[2]);
+        var block = this.yields[index];
+        if (block.invoked) callback.block.invoke(null, false);
+        delete block.yielder;
+        block.detach();
+        delete callback.block;
+        delete callback.parent;
+        break;
+      default:
+        return this.invoke(arguments)
     }
-    var result = this.execute();
-    if (this.locals)
-      for (var local, i = 0; local = this.locals[i]; i++)
-        this.variables.unset(local.name, arguments[i]);
-    this.invoked = false;
+  },
+  invoke: function(args, state) {
+    if (state !== false) {
+      this.invoked = args;
+      this.frozen = true;
+      if (this.locals)
+        for (var local, i = 0; local = this.locals[i]; i++)
+          this.variables.set(local.name, args[i]);
+      delete this.frozen;
+      if (state != null) this.fetch(true);
+      else var result = this.execute()
+    }
+    if (state !== true) {
+      if (args == null) args = this.invoked;
+      if (args === this.invoked || state == null) this.invoked = false;
+      if (state != null) this.fetch(false);
+      if (this.locals)
+        for (var local, i = 0; local = this.locals[i]; i++)
+          this.variables.unset(local.name, args[i]);
+    }
     return result;
   },
   
   process: function(value) {
-    return this.callback;
+    return this.yielder ? this.execute() : this.callback;
   },
   
   onSet: function(value) {
     if (this.output) this.update(value);
-    if (!this.invoked) console.error('onSet block', value)
+    if (this.yielder && this.invoked && this.invoked !== true)
+      this.yielder(value, this.invoked[0], this.invoked[1], this.invoked[2]);
     if (this.parent && !this.invoked) this.parent.set();
   }
 })
