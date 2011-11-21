9626273f189daa35ae69368753296a9446225080
Yaroslaff Fedin <invizko@gmail.com>
2011-11-21 18:52:37 +0800

    Dont let widget be cloned into itself. Find out the parent correctly

--- Source/Action/Clone.js
+++ Source/Action/Clone.js
@@ -21,10 +21,12 @@ provides:
 
 LSD.Action.Clone = LSD.Action.build({
   enable: function(target, parent, before) {
+    if (!target.lsd) target = LSD.Module.DOM.find(target);
     if (target.lsd) var element = target.element || target.toElement(), widget = target;
     else var element = target, widget = LSD.Module.DOM.find(target);
     if (before && !before.nodeType && before !== true) before = null;
-    if (parent && !parent.nodeType && parent !== true) parent = [widget, element];
+    if (parent && !parent.nodeType && parent !== true) 
+      parent = [widget === target ? widget.parentNode : widget, element];
     return LSD.Module.DOM.clone(target, parent, before);
   }
 });
