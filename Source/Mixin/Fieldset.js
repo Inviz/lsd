7fda1c46cc11a7331f809ed82814a10d01c8a2a4
Yaroslaff Fedin <invizko@gmail.com>
2011-11-28 19:02:23 +0800

    Reuse the current value when unsetting it

--- Source/Mixin/Fieldset.js
+++ Source/Mixin/Fieldset.js
@@ -143,7 +143,7 @@ LSD.Mixin.Fieldset = new Class({
     if (!name || !widget.toData) return;
     var callback = function(value, old) {
       if (typeof value == 'undefined') {
-        if (typeof old != 'undefined') this.values.unset(name, old);
+        if (typeof old != 'undefined') this.values.unset(name, widget.getValue());
       } else {
         this.values.set(name, widget.getValue());
       }
