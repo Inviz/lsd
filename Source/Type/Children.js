7d689ec9e58dc5e3633b35c4a87ab6d8e5ae33cc
Yaroslaff Fedin <invizko@gmail.com>
2012-01-27 09:55:03 -0300

    Beef up the LSD.Type.Children.Virtual implementation, a collection that makes a virtual hierarchy in a flat structure of child nodes.

--- Source/Type/Children.js
+++ Source/Type/Children.js
@@ -21,7 +21,7 @@ provides:
 ...
 */
 
-LSD.Type.Children = LSD.Struct.Array({
+LSD.Type.ChildNodes = LSD.Type.Children = LSD.Struct.Array({
   exports: {
     firstChild: 'first',
     lastChild: 'last'
@@ -47,30 +47,42 @@ LSD.Type.Children.prototype.onSet = function(value, index, state, old) {
 };
 LSD.Type.Children.prototype.first = null;
 LSD.Type.Children.prototype.last = null;
+LSD.Type.Children.prototype._onShift = function(index, offset, args, shift) {
+  if (shift === -1 || shift === 1) {
+    var arg = shift === 1 ? args[0] : this[index], children = arg.childNodes;
+    if (children && children.virtual) offset += children.length;
+  }
+  return offset;
+}
 
 LSD.Type.Children.Virtual = LSD.Struct.Array({
   imports: {
-    parentNode: '.parentNode',
-    previousSibling: '.previousSibling'
+    parentNode: '.parentNode'
   },
-  
-  parentNode: function(node) {
-    var children = node.childNodes;
+  parentNode: function(node, old) {
+    var children = (node || old).childNodes;
     var index = children.indexOf(this._parent);
-    var args = [index + 1, 0];
-    for (var i = 0, j = this.childNodes.length; i < j; i++) args.push(this.childNodes[i])
-    children.splice.apply(children, args);
+    if (node) {
+      var args = [index + 1, 0];
+      for (var i = 0, child; child = this[i++];) args.push(child)
+      children.splice.apply(children, args);
+    }
   }
 });
+LSD.Type.Children.Virtual.prototype._onShift = LSD.Type.Children.prototype._onShift;
 LSD.Type.Children.Virtual.prototype.onSet = function(value, index, state, old) {
-  if (old == null) {
-    if (this.parentNode)
-      if (state)
-        this.parentNode.insertBefore(value, (this[this.length - 1] || this._parent).nextSibling)
-      else
-        this.parentNode.removeChild(value);
+  if (old != null) return;
+  var parent = this._parent.parentNode;
+  if (!parent) return;
+  if (value.childNodes && value.childNodes.virtual)
+    value.childNodes[state ? 'set' : 'unset']('parentNode', this._parent) 
+  if (parent.insertBefore) {
+    if (!state) parent.removeChild(value);
+    else parent.insertBefore(value, (this[index - 1] || this._parent).nextSibling)
+  } else {
+    var children = parent.childNodes;
+    if (!state) children.splice(children.indexOf(value), 1)
+    else children.splice(children.indexOf((this[index - 1] || this._parent).nextSibling), 0, value);
   }
-}
-LSD.Type.Children.Virtual = LSD.Struct.Array({
-  
-});
+};
+LSD.Type.Children.Virtual.prototype.virtual = true;
